bc92a016d331bd6421b27692aec9f6099b95ac90
AmelieGersch <amelie.gersch@orange.fr>
2020-01-27 14:09:36 +1300

    save all bp number in one file

---
+++ all_genome_comparaison/count_breakpoints/README.md
@@ -0,0 +1,5 @@
+Input : paf file, result from Minimap2
+count number of breakpoint between the two strains 
+select synteny aligment from correct size and quality
+
+all results are save in one file 
---
+++ all_genome_comparaison/count_breakpoints/save_results.sh
@@ -0,0 +1,20 @@
+#save results of Rrun
+#number of breakpoints in each alignment
+
+cd ~/fungalgenome/all_genome_comparaison/count_breakpoints
+echo results for query target number_breakpoints > all_count_breakpoint.txt
+echo Eel728 Eam 24 >> all_count_breakpoint.txt
+echo Eel732 Eam 24 >> all_count_breakpoint.txt
+echo Efe Eam 28 >> all_count_breakpoint.txt
+echo Ety Eam 20 >> all_count_breakpoint.txt
+echo Eam Ecl 20 >> all_count_breakpoint.txt
+echo Eel728 Ecl 5 >> all_count_breakpoint.txt
+echo Eel732 Ecl 9 >> all_count_breakpoint.txt
+echo Efe Ecl 9 >> all_count_breakpoint.txt
+echo Ety Ecl 18 >> all_count_breakpoint.txt
+echo Eel732 Eel728 6 >> all_count_breakpoint.txt
+echo Eel728 Efe 18 >> all_count_breakpoint.txt
+echo Eel732 Efe 22 >> all_count_breakpoint.txt
+echo Ety Efe 8 >> all_count_breakpoint.txt
+echo Eel728 Ety 6 >> all_count_breakpoint.txt 
+echo Eel732 Ety 10 >> all_count_breakpoint.txt
